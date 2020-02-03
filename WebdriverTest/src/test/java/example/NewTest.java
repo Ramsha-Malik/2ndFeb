362498bdebf4c9582189c6765190f0e46df10439
Ramsha <Ramsha@DESKTOP-G5O3F3J.speedport.ip>
2020-02-03 20:55:27 +0100

    Commit1

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,31 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	private WebDriver driver;
+  @Test
+  public void f() {
+	  driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+  }
+  @BeforeTest
+  public void b1()
+  {
+	  System.setProperty("webdriver.chrome.driver","D:\\\\chromedriver.exe");
+	  driver=new ChromeDriver();
+  }
+  @AfterTest
+  public void b2()
+  {
+	  driver.close();
+  }
+  
+  
+}
