1f228d169573bf87c988a9bb1eaf0dfeba1523b0
Ramsha <Ramsha@DESKTOP-G5O3F3J.speedport.ip>
2020-02-05 19:08:16 +0100

    Commit3-error

--- WebdriverTest/src/test/java/example/NewTest.java
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -13,7 +13,7 @@ public class NewTest {
   public void f() {
 	  driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		Assert.assertTrue(title.contains("Demo Guru99 ")); 		
 		
   }
   @BeforeTest
